e09347da3cb3d49638475c3c910b1c8cce830f68
wz1310 <a002041zano@gmail.com>
2026-02-13 19:27:58 +0700

    update

--- custom_sale_stock_warning/models/m_product.py
+++ custom_sale_stock_warning/models/m_product.py
@@ -8,17 +8,6 @@ from odoo.exceptions import UserError, ValidationError
 class ProductProduct(models.Model):
     _inherit = 'product.template'
 
-    # def name_get(self):
-    #     print("ahaaaaaaaaaaaaaaaa")
-    #     result = []
-    #     for product in self:
-    #         stok = product.qty_available
-    #         name = f"{product.display_name} (Stok: {stok})"
-    #         if stok <= 0:
-    #             name = f"⚠️ {product.display_name} (HABIS)"
-    #         result.append((product.id, name))
-    #     return result
-
     @api.depends('name', 'default_code')
     def _compute_display_name(self):
         action = self.env.context.get('params', {}).get('action')
@@ -27,9 +16,15 @@ class ProductProduct(models.Model):
         for template in self:
             # action = template.env.context.get('params', {}).get('action')
             # print(action)
-            product = template.env['product.product'].browse(template.id)
-            print(product.name)
-            template.display_name = False if not template.name else (
-                '{}{}'.format(
-                    template.default_code and '[%s] ' % template.default_code or '', template.name
-                    ))
+            product = template.env['product.product'].search([('product_tmpl_id','in',[template.id])])
+            on_hande = sum(product.mapped('qty_available'))
+            if on_hande > 0:
+                template.display_name = False if not template.name else (
+                    '{}{}'.format(
+                        template.default_code and '[%s] ' % template.default_code or '', template.name
+                        ))
+            else:
+                template.display_name = False if not template.name else (
+                    '{}{}'.format(
+                        template.default_code and '⚠️ [%s] ' % template.default_code or '', template.name
+                        ))
